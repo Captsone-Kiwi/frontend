123e0cdc0a285b43565d419b964240d27653b0a8
KimChanMi <54922827+kimchanmiii@users.noreply.github.com>
2022-04-27 03:12:35 +0900

    login home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as style from "./styles";
 import { useNavigate } from "react-router-dom";
+import authAPI from "../../api/authAPI";
 
 function Home() {
   const navigator = useNavigate();
+  const [user, setUserInfo] = useState();
+
+  const checkUser = async () => {
+    await authAPI
+      .getUser()
+      .then((res) => {
+        setUserInfo(res.data.data);
+        navigator("/interviewlist");
+        console.log("checkUser result", res);
+      })
+      .catch((error) => {
+        alert("로그인을 해야 이용할 수 있습니다.");
+        navigator("/login");
+        console.log("checkUser error", error);
+      });
+  };
 
   return (
     <style.Container>
@@ -27,9 +44,7 @@ function Home() {
             for non-face-to-face interviews through the device-free web
           </style.mainTextDetail>
           <style.btnContainer>
-            <style.Button onClick={() => navigator("/interviewlist")}>
-              Start Kiwi
-            </style.Button>
+            <style.Button onClick={checkUser}>Start Kiwi</style.Button>
           </style.btnContainer>
         </style.textContainer>
       </style.mainContainer>
