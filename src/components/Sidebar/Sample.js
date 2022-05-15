d8ea09e635d32844f0c62d422507bc5ebcdbd77b
KimChanMi <54922827+kimchanmiii@users.noreply.github.com>
2022-05-16 02:36:35 +0900

    .

--- src/components/Sidebar/Sample.js
+++ src/components/Sidebar/Sample.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useState, useContext } from 'react';
-import ResumeAPI from "../../api/ResumeAPI";
+import React, { useEffect, useState, useContext } from "react";
+import ResumeAPI from "../../api/resumeAPI";
 
 function Sample(props) {
-  
+  const [embedURL, setEmbedURL] = useState(
+    `http://localhost:8000/getResume?name=${props.name}`
+  );
 
-  const [embedURL, setEmbedURL] = useState(`http://localhost:8000/getResume?name=${props.name}`)
-
-  return (
-      <iframe src={embedURL} width="400px" height='100%'></iframe>
-  )
+  return <iframe src={embedURL} width="400px" height="100%"></iframe>;
 }
 
 export default Sample;
