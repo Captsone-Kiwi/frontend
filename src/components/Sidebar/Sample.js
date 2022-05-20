c27862b5c4fb4f1d0f5b4e735ce1387e05d72662
Baek-sohyeon <kitty224@kookmin.ac.kr>
2022-05-20 15:34:44 +0900

    Update IP

--- src/components/Sidebar/Sample.js
+++ src/components/Sidebar/Sample.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState, useContext } from 'react';
 function Sample(props) {
   
 
-  const [embedURL, setEmbedURL] = useState(`http://localhost:8000/getResume?name=${props.name}`)
+  const [embedURL, setEmbedURL] = useState(`http://35.174.145.15/getResume?name=${props.name}`)
 
   return (
       <iframe src={embedURL} width="400px" height='100%'></iframe>
